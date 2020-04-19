988b0ff447795ff83fae89512bde8b2f731d78fc
Pranav Nagarajan <pranav.njn@berkeley.edu>
2020-04-19 00:29:36 -0700

    Adding interactive notebook for data exploration

--- Saha_Hybrid_Method.py
+++ Saha_Hybrid_Method.py
@@ -6,14 +6,18 @@
 #
 ###################################################################################################
 
-import numpy as np
-import pandas as pd
+from pwkit.pdm import pdm
+from astropy.timeseries import LombScargle
 
-def phase_dispersion_minimization(data):
-    return
+def phase_dispersion_minimization(times, magnitudes, uncertainties, frequencies, nbins = 10):
+    lafler_kinman = pdm(times, magnitudes, uncertainties, 1 / frequencies, nbins)
+    return lafler_kinman[0:3]
 
-def lomb_scargle_analysis(data):
-    return
+def lomb_scargle_analysis(times, magnitudes, uncertainties):
+    frequency, power = LombScargle(times, magnitudes, uncertainties).autopower()
+    return [frequency, power]
 
-def hybrid_statistic(data):
-    return
+def hybrid_statistic(times, magnitudes, uncertainties):
+    frequencies, pi = lomb_scargle_analysis(times, magnitudes, uncertainties)
+    theta = phase_dispersion_minimization(times, magnitudes, uncertainties, frequencies)[0]
+    return [frequencies, 2 * pi / theta]
