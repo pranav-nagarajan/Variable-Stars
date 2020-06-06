ab76fb1d5e3988ea5ccd34512f48009f72fb3d96
Pranav Nagarajan <pranav.njn@berkeley.edu>
2020-06-05 23:36:15 -0700

    Creating hierarchical Bayes model

--- cepheid_mcmc.py
+++ cepheid_mcmc.py
@@ -13,19 +13,28 @@ mcmc_args = mcmc_parser.parse_args()
 lin_reg_table = pd.read_csv(mcmc_args.cepheid_data)
 number_of_cpus = mcmc_args.number_of_cpus
 
+log_period =  lin_reg_table['Log Period'].values
+rel_metal = lin_reg_table['Relative Metallicity'].values
+obs_mag = lin_reg_table['Wesenheit Magnitude'].values
+galaxy_id = lin_reg_table['Galaxy Code'].values
+
 cepheid_model = pm.Model()
 
 with cepheid_model:
 
-    mod = pm.Normal('mod', mu = 0, sigma = 10)
+    mod_mu = pm.Normal('mod_mu', mu = 0, sigma = 10)
+    mod_sig = pm.HalfNormal('mod_sigma', sigma = 1)
+
+    mod = pm.Normal('mod', mu = mod_mu, sigma = mod_sig, shape = 20)
+
     zpw = pm.Normal('zpw', mu = 0, sigma = 10)
     bw = pm.Normal('bw', mu = 0, sigma = 10)
     zw = pm.Normal('zw', mu = 0, sigma = 10)
     sigma = pm.HalfNormal('sigma', sigma = 1)
 
-    mag = mod + zpw + bw * lin_reg_table['Log Period'].values + zw * lin_reg_table['Relative Metallicity'].values
+    mag = mod[galaxy_id] + zpw + bw * log_period + zw * rel_metal
 
-    obs = pm.Normal('obs', mu = mag, sigma = sigma, observed = lin_reg_table['Wesenheit Magnitude'].values)
+    obs = pm.Normal('obs', mu = mag, sigma = sigma, observed = obs_mag)
 
 with cepheid_model:
 
