05b4fa84eba214cac5e1b481ae1081d95e2d4a82
Pranav Nagarajan <pranav.njn@berkeley.edu>
2020-06-11 17:00:47 -0700

    Adding measurement errors and reducing free parameters

--- cepheid_mcmc.py
+++ cepheid_mcmc.py
@@ -4,6 +4,7 @@ import multiprocessing as mp
 import numpy as np
 import pandas as pd
 import pymc3 as pm
+import theano.tensor as tt
 
 mcmc_parser = argparse.ArgumentParser(description = "Helper for parallel processing.")
 mcmc_parser.add_argument('number_of_cpus', metavar = 'N', type = int, help = "Number of processes to use.")
@@ -18,21 +19,26 @@ rel_metal = lin_reg_table['Relative Metallicity'].values
 obs_mag = lin_reg_table['Wesenheit Magnitude'].values
 galaxy_id = lin_reg_table['Galaxy Code'].values
 
+mask = np.array(lin_reg_table['Galaxy'] == 'N4258')
+
 cepheid_model = pm.Model()
 
 with cepheid_model:
 
     mod = pm.Uniform('mod', lower = -10, upper = 10, shape = 20)
+    rel_mod = tt.set_subtensor(mod[galaxy_id][mask.nonzero()], 0)
 
     zpw = pm.Uniform('zpw', lower = 20, upper = 30)
     bw = pm.Uniform('bw', lower = -10, upper = 10)
     zw = pm.Uniform('zw', lower = -10, upper = 10)
 
-    sigma = pm.HalfNormal('sigma', sigma = 1)
+    err = lin_reg_table['Uncertainty in Magnitude (F160W)'].values
+    var = pm.HalfNormal('var', sd = 1)
+    sigma = np.sqrt(err**2 + var)
 
-    mag = mod[galaxy_id] + zpw + bw * log_period + zw * rel_metal
+    mag = rel_mod + zpw + bw * log_period + zw * rel_metal
 
-    obs = pm.Normal('obs', mu = mag, sigma = sigma, observed = obs_mag)
+    obs = pm.Normal('obs', mu = mag, sd = sigma, observed = obs_mag)
 
 with cepheid_model:
     cepheid_trace = pm.sample(cores = number_of_cpus, return_inferencedata = False)
