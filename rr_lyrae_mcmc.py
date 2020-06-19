752f31f8fe6a6defc3ed7f1a2c5c0d50304d6a49
Pranav Nagarajan <pranav.njn@berkeley.edu>
2020-06-18 22:53:29 -0700

    Adding multiple galaxies to MCMC model

--- rr_lyrae_mcmc.py
+++ rr_lyrae_mcmc.py
@@ -7,45 +7,56 @@ import pymc3 as pm
 import theano.tensor as tt
 
 mcmc_parser = argparse.ArgumentParser(description = "Helper for parallel processing.")
-mcmc_parser.add_argument('number_of_cpus', metavar = 'N', type = int, help = "Number of processes to use.")
-mcmc_parser.add_argument('rr_lyrae_data', type = str, help = "Data for RR Lyrae stars in a specific galaxy.")
-mcmc_parser.add_argument('zero_point', type = float, help = "Theoretical zero point.")
-mcmc_parser.add_argument('zp_error', type = float, help = "Uncertainty in theoretical zero point.")
-mcmc_parser.add_argument('mean_metallicity', type = float, help = "Mean metallicity in galaxy.")
-mcmc_parser.add_argument('sd_metallicity', type = float, help = "Standard deviation of metallicity in galaxy.")
+mcmc_parser.add_argument('--num_cpus', type = int, help = "Number of processes to use.")
+mcmc_parser.add_argument('--data', action = append, type = str, help = "Data for RR Lyrae stars.")
+mcmc_parser.add_argument('--zero_point', nargs = 2, type = float, help = "Theoretical zero point.")
+mcmc_parser.add_argument('--metal', nargs = 2, action = append, type = float, help = "Mean metallicity in galaxy.")
 mcmc_args = mcmc_parser.parse_args()
 
-lin_reg_table = pd.read_csv(mcmc_args.rr_lyrae_data)
-number_of_cpus = mcmc_args.number_of_cpus
+number_of_cpus = mcmc_args.num_cpus
 
-mean_metal, sd_metal = mcmc_args.mean_metallicity, mcmc_args.sd_metallicity
-zp, zp_error = mcmc_args.zero_point, mcmc_args.zp_error
+lin_reg_tables = []
+for table in mcmc_args.data:
+    lin_reg_tables.append(pd.read_csv(data))
 
-log_period =  lin_reg_table['Log Period'].values
-obs_mag = lin_reg_table['Wesenheit Magnitude'].values
+metals = mcmc_args.metal
+zp, zp_error = mcmc_args.zero_point[0], mcmc_args.zero_point[1]
 
-num_stars = len(lin_reg_table['Star'].unique())
-star_id = lin_reg_table['Star Code'].values
+log_periods = []
+obs_mags = []
+star_nums = []
+star_ids = []
+
+for lin_reg_table in lin_reg_tables:
+    log_periods.append(lin_reg_table['Log Period'].values)
+    obs_mags.append(lin_reg_table['Wesenheit Magnitude'].values)
+    star_nums.append(lin_reg_table['Star'].unique())
+    star_ids.append(lin_reg_table['Star Code'].values)
 
 rr_lyrae_model = pm.Model()
 
 with rr_lyrae_model:
 
-    modulus = pm.Normal('modulus', mu = 0, sd = 10)
+    modulus = pm.Normal('modulus', mu = 20, sd = 10)
+    sigma = pm.HalfNormal('sigma', sd = 1)
 
     zero_point = pm.Normal('zero_point', mu = zp, sd = zp_error)
     period_slope = pm.Normal('period_slope', mu = 0, sd = 10)
     metal_slope = pm.Normal('metallicity_slope', mu = 0, sd = 10)
 
-    metallicity = pm.Normal('metallicity', mu = mean_metal, sd = sd_metal, shape = num_stars)
+    mag, obs_mag = tt.vector(), tt.vector()
 
-    sigma = pm.HalfNormal('sigma', sd = 1)
+    for i in range(len(log_periods)):
 
-    mag = modulus + zero_point + period_slope * log_period + metal_slope * metallicity[star_id]
+        metallicity = pm.Normal('metallicity', mu = metals[i][0], sd = metals[i][1], shape = star_nums[i])
+        magnitude = modulus + zero_point + period_slope * log_periods[i] + metal_slope * metallicity[star_ids[i]]
+        mag = pm.math.concatenate((mag, magnitude))
+        obs_mag = pm.math.concatenate((obs_mag, obs_mags[i]))
 
-    obs = pm.Normal('obs', mu = mag, sd = sigma, observed = obs_mag)
+    obs = pm.Normal('obs', mu = magnitude, sd = sigma, observed = obs_mag)
 
 with rr_lyrae_model:
+
     rr_lyrae_trace = pm.sample(cores = number_of_cpus)
 
 pickle.dump(rr_lyrae_trace, open('rr_lyrae.pkl', 'wb'))
