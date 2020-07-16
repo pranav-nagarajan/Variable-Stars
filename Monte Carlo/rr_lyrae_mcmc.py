9af4828659ebc245d686c26a86fa97ad65b23736
Pranav Nagarajan <pranav.njn@berkeley.edu>
2020-07-15 17:21:15 -0700

    Fixing tensor concatenation

--- Monte Carlo/rr_lyrae_mcmc.py
+++ Monte Carlo/rr_lyrae_mcmc.py
@@ -63,11 +63,14 @@ with rr_lyrae_model:
         magnitudes.append(modulus[i] + zero_point + period_slope * log_periods[i] +
                           metal_slope * metal[star_ids[i]])
 
+    calibrations = []
+
     for i in range(len(calibrate['Star Code'])):
 
-        magnitudes.append(field_moduli[i] + field_zero_point + period_slope * field_periods[i] +
-                          metal_slope * field_metal[i])
+        calibrations.append(field_moduli[i] + field_zero_point + period_slope * field_periods[i] +
+                            metal_slope * field_metal[i])
 
+    magnitudes.append(calibrations)
     modeled, observed = pm.math.concatenate(magnitudes), pm.math.concatenate(obs_mags)
     obs = pm.Normal('obs', mu = modeled, sd = sigma, observed = observed)
 
