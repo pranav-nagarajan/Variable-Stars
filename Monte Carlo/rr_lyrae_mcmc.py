34780ed1b56e5a15c2acc036f36ac3c6d6a234cf
Pranav Nagarajan <pranav.njn@berkeley.edu>
2020-08-28 20:07:49 -0700

    Updating plots in MCMC notebook

--- Monte Carlo/rr_lyrae_mcmc.py
+++ Monte Carlo/rr_lyrae_mcmc.py
@@ -112,6 +112,6 @@ with rr_lyrae_model:
 
 with rr_lyrae_model:
 
-    rr_lyrae_trace = pm.sample(cores = number_of_cpus, draws = 2500, tune = 10000)
+    rr_lyrae_trace = pm.sample(cores = number_of_cpus)
 
 pickle.dump(rr_lyrae_trace, open('rr_lyrae.pkl', 'wb'))
