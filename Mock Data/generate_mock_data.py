a2bcddf158801f3b002f9885446d7a4f17e753ac
Pranav Nagarajan <pranav.njn@berkeley.edu>
2020-09-25 09:46:02 -0700

    Increasing number of stars in galaxy zero

--- Mock Data/generate_mock_data.py
+++ Mock Data/generate_mock_data.py
@@ -12,7 +12,7 @@ sig_feh = 0.5 # width of each galaxy's MDF, in dex
 
 mus = np.random.uniform(18, 25, N_gals) # distance moduli
 
-N_stars_1 = np.random.randint(1, 5) # number of stars in each galaxy
+N_stars_1 = np.random.randint(2, 5) # number of stars in each galaxy
 N_stars_2 = np.random.randint(10, 50)
 N_stars_3 = np.random.randint(100, 500)
 N_stars = np.array([N_stars_1, N_stars_2, N_stars_3])
