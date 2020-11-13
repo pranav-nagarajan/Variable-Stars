a81514568708b190a65d7947c2df45264423fac4
Pranav Nagarajan <pranav.njn@berkeley.edu>
2020-11-13 08:23:11 -0800

    Increasing time limit

--- Monte Carlo/rr_lyrae_mcmc.sh
+++ Monte Carlo/rr_lyrae_mcmc.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=24
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --output=test_job_%j.out
 #SBATCH --error=test_job_%j.err
 #SBATCH --mail-type=END,FAIL
