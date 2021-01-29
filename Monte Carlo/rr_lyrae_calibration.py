09c963dfee03ee2c2e8049a6bf1c848543e83a5b
Pranav Nagarajan <pranav.njn@berkeley.edu>
2021-01-28 21:55:14 -0800

    Updating model for calibration stars

--- Monte Carlo/rr_lyrae_calibration.py
+++ Monte Carlo/rr_lyrae_calibration.py
@@ -18,24 +18,44 @@ field_periods = calibrate['Log Period'].values
 field_moduli = calibrate['Distance Modulus'].values
 field_metal = calibrate['Metallicity'].values
 
-observed = calibrate['Wesenheit Magnitude'].values
+observed = []
+observed.append(calibrate['B-V Wesenheit Magnitude'].values)
+observed.append(calibrate['V-I Wesenheit Magnitude'].values)
+observed = pm.math.concatenate(observed)
 
-field_mag_err = calibrate['Uncertainty in Wesenheit Magnitude'].values
+errors = []
+field_mag_err_BV = calibrate['Uncertainty in B-V Wesenheit Magnitude'].values
+field_mag_err_VI = calibrate['Uncertainty in V-I Wesenheit Magnitude'].values
 field_mod_err = calibrate['Uncertainty in Distance Modulus'].values
+errors.append(np.sqrt(field_mag_err_BV**2 + field_mod_err**2))
+errors.append(np.sqrt(field_mag_err_VI**2 + field_mod_err**2))
+errors = np.hstack(errors)
 
 rr_lyrae_model = pm.Model()
 
 with rr_lyrae_model:
 
     sigma = pm.HalfNormal('sigma', sd = 1)
-    total_err = np.sqrt(sigma**2 + field_mag_err**2 + field_mod_err**2)
+    total_err = np.sqrt(sigma**2 + errors**2)
 
-    zero_point = pm.Normal('calibration_point', mu = 0, sd = 10)
-    period_slope = pm.Normal('period_slope', mu = 0, sd = 10)
-    metal_slope = pm.Normal('metallicity_slope', mu = 0, sd = 10)
+    zero_point_BV = pm.Normal('calibration_point_BV', mu = 0, sd = 10)
+    period_slope_BV = pm.Normal('period_slope_BV', mu = 0, sd = 10)
+    metal_slope_BV = pm.Normal('metallicity_slope_BV', mu = 0, sd = 10)
 
-    modeled = (zero_point + field_moduli + period_slope * (field_periods + 0.3)
-               + metal_slope * (field_metal + 1.36))
+    zero_point_VI = pm.Normal('calibration_point_VI', mu = 0, sd = 10)
+    period_slope_VI = pm.Normal('period_slope_VI', mu = 0, sd = 10)
+    metal_slope_VI = pm.Normal('metallicity_slope_VI', mu = 0, sd = 10)
+
+    modeled_BV = (zero_point_BV + field_moduli + period_slope_BV * (field_periods + 0.3)
+                  + metal_slope_BV * (field_metal + 1.36))
+
+    modeled_VI = (zero_point_VI + field_moduli + period_slope_VI * (field_periods + 0.3)
+                  + metal_slope_VI * (field_metal + 1.36))
+
+    magnitudes = []
+    magnitudes.append(modeled_BV)
+    magnitudes.append(modeled_VI)
+    modeled = pm.math.concatenate(magnitudes)
 
     obs = pm.Normal('obs', mu = modeled, sd = total_err, observed = observed)
 
