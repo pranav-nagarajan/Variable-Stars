bc02527846f1d483a376a64520638323af1162b7
Pranav Nagarajan <pranav.njn@berkeley.edu>
2021-05-21 13:35:16 -0700

    Updating plots in MCMC notebook

---
+++ Monte Carlo/rr_lyrae_outlier.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=mcmc
+#SBATCH --account=co_dweisz
+#SBATCH --partition=savio2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=24
+#SBATCH --time=02:00:00
+#SBATCH --output=test_job_%j.out
+#SBATCH --error=test_job_%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=pranav.njn@berkeley.edu
+
+## Command(s) to run:
+module load python/3.6
+echo "Starting MCMC for RR Lyrae P-L Relation..."
+python rr_lyrae_mcmc.py --num_cpus $SLURM_CPUS_PER_TASK --data and_eleven_mcmc_test.csv --galaxies galaxies_outlier.csv --calibrate milky_way_VI.csv
