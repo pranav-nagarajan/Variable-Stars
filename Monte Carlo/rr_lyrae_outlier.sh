69f3a13d74333a6623f6a669828c3d8e643ff3fb
Pranav Nagarajan <pranav.njn@berkeley.edu>
2021-05-24 19:30:58 -0700

    Investigating Ursa Major II

--- Monte Carlo/rr_lyrae_outlier.sh
+++ Monte Carlo/rr_lyrae_outlier.sh
@@ -14,4 +14,4 @@
 ## Command(s) to run:
 module load python/3.6
 echo "Starting MCMC for RR Lyrae P-L Relation..."
-python rr_lyrae_mcmc.py --num_cpus $SLURM_CPUS_PER_TASK --data sculptor_mcmc.csv --galaxies galaxies_outlier.csv --calibrate milky_way_VI.csv
+python rr_lyrae_mcmc.py --num_cpus $SLURM_CPUS_PER_TASK --data ursa_major_two_mcmc.csv --galaxies galaxies_outlier.csv --calibrate milky_way_VI.csv
